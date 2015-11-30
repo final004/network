211c39611785673715a9369980ad546fee6fc639
bit-user <bit-user@bit>
2015-11-30 11:51:09 +0900

    LSLookup commit

---
+++ src/com/hanains/network/test/TCPServer.java
@@ -0,0 +1,54 @@
+package com.hanains.network.test;
+
+import java.io.IOException;
+import java.net.InetAddress;
+import java.net.InetSocketAddress;
+import java.net.ServerSocket;
+import java.net.Socket;
+
+public class TCPServer {
+	
+	private static final int PORT = 5050;
+	
+	public static void main(String[] args) {
+		ServerSocket serverSocket = null;
+		
+		try {
+			// 1.서버 소켓 생성
+			serverSocket = new ServerSocket();
+			
+			// 2.바인딩
+			InetAddress inetAddress = InetAddress.getLocalHost();
+			String localhost = inetAddress.getHostAddress();
+			serverSocket.bind(new InetSocketAddress(localhost, PORT));
+			System.out.println("[서버]바인딩 " + localhost + ":" + PORT);
+			
+			// 3.연결 요청 대기(accept)
+			Socket socket = serverSocket.accept();
+			
+			// 4.연결 성공
+			InetSocketAddress inetSocketAddress = (InetSocketAddress) socket.getRemoteSocketAddress();
+			String remoteHostAddress = inetSocketAddress.getAddress().getHostAddress();
+			int remoteHostPort = inetSocketAddress.getPort();
+			System.out.println("[서버]연결됨 from " + remoteHostAddress + ":" + remoteHostPort);
+			
+			// 7.소켓 닫기
+			if(socket.isClosed() == false){
+				socket.close();
+			}
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} finally{
+			// 서버 소켓 닫기
+			if(serverSocket != null && serverSocket.isClosed() == false){
+				try {
+					serverSocket.close();
+				} catch (IOException e) {
+					// TODO Auto-generated catch block
+					e.printStackTrace();
+				}
+			}
+		}
+	}
+}
--- src/com/hanains/network/util/NSLookup.java
+++ src/com/hanains/network/util/NSLookup.java
@@ -2,21 +2,26 @@ package com.hanains.network.util;
 
 import java.net.InetAddress;
 import java.net.UnknownHostException;
+import java.util.Arrays;
 import java.util.Scanner;
 
 public class NSLookup {
 	public static void main(String[] args) {
 		Scanner scanner = new Scanner(System.in);
-		String hostname = scanner.nextLine();
+		String hostname;
 		InetAddress[] inetAddress;
 		while(true){
+			System.out.print("> ");
+			hostname = scanner.nextLine();
 			if("exit".equals(hostname)){
 				System.exit(0);
 			}
 			else{
 				try {
 					inetAddress = InetAddress.getAllByName(hostname);
-					System.out.println(inetAddress);
+					for(int i=0; i<inetAddress.length; i++){
+						System.out.println(InetAddress.getByName(hostname).getHostName() + " : " + InetAddress.getByName(hostname).getHostAddress());
+					}
 				} catch (UnknownHostException e) {
 					// TODO Auto-generated catch block
 					e.printStackTrace();
